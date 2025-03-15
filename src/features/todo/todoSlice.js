19204036f18aa6a16630069799098eb702c1858a
kavi-ta <kavisharma2807@gmail.com>
2025-03-15 12:37:26 +0530

    setup-initial commit

---
+++ src/features/todo/todoSlice.js
@@ -0,0 +1,27 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: 1, text: "Hello World" }],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+  },
+});
+
+// export individual reducers
+export const { addTodo, removeTodo } = todoSlice.actions;
+// return entire reducer to register in store
+export default todoSlice.reducer;
